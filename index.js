89393063fefb8547e29ee06b9ffa0e25798fffc9
Mukarram Haq <mukarram796@gmail.com>
2024-09-07 19:14:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const btnCardShare = document.getElementById('btn_card_share')
 const cardShare = document.getElementById('card_share')
 
-// Escuchamos evento click en botón compartir
+
 btnCardShare.addEventListener('click', () => {
-    // Conmutamos la clase que muestra el componente compartir
+   
     cardShare.classList.toggle('fade_in')
 })
 
-// Escuchamos el evento del mouse cuando deja la zona compartir
 cardShare.addEventListener('mouseleave', () => {
-    // Removemos la clase para que se oculte el componente compartir
     cardShare.classList.remove('fade_in')
 })
